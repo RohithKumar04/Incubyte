2aea7b1b56d7c3c20071ea2972d45b6484988246
RohithKumar04 <vkrohithkumar@gmail.com>
2024-02-17 11:43:10 +0530

    Tests for basic add + egde case of empty string

---
+++ src/main/java/com/incubyte/application/StringCalc.java
@@ -0,0 +1,10 @@
+package com.incubyte.application;
+
+public class StringCalc {
+
+    public int calculate(String numbers) {
+        // Code to be added here
+
+        return 0;
+    }
+}
--- src/test/java/com/incubyte/application/ApplicationTests.java
+++ src/test/java/com/incubyte/application/ApplicationTests.java
@@ -1,13 +1,42 @@
 package com.incubyte.application;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApplicationTests {
 
-	@Test
-	void contextLoads() {
+	private StringCalc stringCalc;
+
+	@BeforeEach
+	void setup() {
+		stringCalc = new StringCalc();
 	}
 
+	@Test
+	void whenEmpty_defaultToZero() {
+
+		int result=stringCalc.calculate("");
+		Assertions.assertEquals(result, 0);
+
+	}
+
+	@Test
+	void whenSizeIsOne_returnAsNumber() {
+
+		int result=stringCalc.calculate("1");
+		Assertions.assertEquals(result, 1);
+
+	}
+
+	@Test
+	void whenCommaSeparated_AddNumbers() {
+
+		int result=stringCalc.calculate("1,2");
+		Assertions.assertEquals(result, 3);
+
+	}
 }
